54b321756fee19864ba5bb31b1f7fed091ecff30
Cameron Kilgore <ghostfreeman@gmail.com>
2015-01-30 10:01:45 -0500

    Implemented date format logic (timezone).

--- js/twc_block_demo.js
+++ js/twc_block_demo.js
@@ -1,5 +1,7 @@
 Drupal.behaviors.twc_block_demo_hide_last_login = {
   attach: function(context, settings) {
-    jQuery('#block-twc-block-demo-user-last-login').delay(10000).slideUp();
+    if (jQuery('#block-twc-block-demo-user-last-login').length) {
+      jQuery('#block-twc-block-demo-user-last-login').delay(10000).slideUp();
+    }
   }
 };
